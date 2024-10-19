7b1ed97559311c744de56d8278db7b0d06e3769e
wellrestedmango <ericfassbender@proton.me>
2024-10-19 11:45:04 -0500

    setting up my tabs

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,54 +1,53 @@
-import { useState } from 'react'
+import { useState, React } from 'react'
 import '../App.css'
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
     Container,
+    Tab,
+    Tabs,
+    TabList,
+    TabPanel,
+    TabPanels
   } from '@chakra-ui/react'
 
 export const Resources=() => {
     return (
-        <fieldset id="hello">
-            <p>hello</p>
-            <p>world</p>
-        <Accordion>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                Section 1 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-      </fieldset>
+        <Container>
+            <h1>
+                Basic Resources Page
+            </h1>
+            <h3>
+                More explaination as to what is going on here
+            </h3>
+            <h4>
+                Here are some basic links to get you started
+            </h4>
+            <Tabs>
+                <TabList>
+                    <Tab>FreeCodeCamp</Tab>
+                    <Tab>Udemy</Tab>
+                    <Tab>GitHub</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <p>Click <a href="freecodecamp.org" target="_blank">here</a> to get started</p>
+                        <ul>
+                            <li><a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" target="_blank">Javascript</a></li>
+                            <li><a href="https://www.freecodecamp.org/learn/python-for-everybody/" target="_blank">Python</a></li>
+                            <li><a href="https://www.freecodecamp.org/learn/front-end-development-libraries/" target="_blank">Front end dev libraries</a></li>
+                        </ul>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Click <a href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" target="_blank">here</a> to get started</p>
+                        <ul>
+                            <li>
+
+                            </li>
+                        </ul>
+                    </TabPanel>
+
+                </TabPanels>
+            </Tabs>
+            
+        </Container>
     )
 }
