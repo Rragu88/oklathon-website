575b4d57bc9a27d3724831a806d239098b4f8a63
wellrestedmango <ericfassbender@proton.me>
2024-10-19 13:59:33 -0500

    changing p to text
    changing p to text to fix some alignment issues

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -44,7 +44,7 @@ export const Resources=() => {
                 </TabList>
                 <TabPanels display="flex" justifyContent="flex-start">
                     <TabPanel>
-                        <p>Click <Link href="freecodecamp.org" color="teal.500" target="_blank">here</Link> to get started</p>
+                        <Text textAlign="left">Click <Link href="freecodecamp.org" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
                             <ListItem><Link href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" color="teal.500" target="_blank">Javascript Algorithms</Link></ListItem>
                             <ListItem><Link href="https://www.freecodecamp.org/learn/python-for-everybody/" color="teal.500" target="_blank">Python for everyone</Link></ListItem>
@@ -52,7 +52,7 @@ export const Resources=() => {
                         </UnorderedList>
                     </TabPanel>
                     <TabPanel>
-                        <p>Click <Link href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" color="teal.500" target="_blank">here</Link> to get started</p>
+                        <Text textAlign="left">Click <Link href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
                             <ListItem>
                                 <Link href="https://www.udemy.com/course/javascript-code-exercise/" color="teal.500" target="_blank">Intro to JavaScript</Link>
@@ -69,7 +69,7 @@ export const Resources=() => {
                         </UnorderedList>
                     </TabPanel>
                     <TabPanel>
-                        <p>Click <Link href="https://github.com" color="teal.500" target="_blank">here</Link> to get started</p>
+                        <Text textAlign="left">Click <Link href="https://github.com" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
                             <ListItem>
                                 <Link href="https://pages.github.com/" color="teal.500" target="_blank">GitHub Pages</Link>
@@ -86,7 +86,7 @@ export const Resources=() => {
                         </UnorderedList>
                     </TabPanel>
                     <TabPanel>
-                        <p>Curated Collections</p>
+                        <Text textAlign="left">Curated Collections</Text>
                         <UnorderedList>
                             <ListItem>
                                 <Link href="https://github.com/MunGell/awesome-for-beginners" color="teal.500" target="_blank">Awesome for beginners</Link>
