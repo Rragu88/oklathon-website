690cb41670b15114cf02aabbe96ce5d927ba16ec
wellrestedmango <ericfassbender@proton.me>
2024-10-19 14:15:44 -0500

    Adding CSS for black background
    and finally fixing text alignment across the board

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -24,7 +24,7 @@ import {
 export const Resources=() => {
     return (
         <Container
-        // maxW="1100px" 
+        maxW="1100px" 
         display="flex"
         justifyContent="flex-start"
         flexDir="column"
@@ -46,25 +46,25 @@ export const Resources=() => {
                     <TabPanel>
                         <Text textAlign="left">Click <Link href="freecodecamp.org" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
-                            <ListItem><Link href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" color="teal.500" target="_blank">Javascript Algorithms</Link></ListItem>
-                            <ListItem><Link href="https://www.freecodecamp.org/learn/python-for-everybody/" color="teal.500" target="_blank">Python for everyone</Link></ListItem>
-                            <ListItem><Link href="https://www.freecodecamp.org/learn/front-end-development-libraries/" color="teal.500" target="_blank">Front end dev libraries</Link></ListItem>
+                            <Text textAlign="left"><ListItem><Link href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" color="teal.500" target="_blank">Javascript Algorithms</Link></ListItem></Text>
+                            <Text textAlign="left"><ListItem><Link href="https://www.freecodecamp.org/learn/python-for-everybody/" color="teal.500" target="_blank">Python for everyone</Link></ListItem></Text>
+                            <Text textAlign="left"><ListItem><Link href="https://www.freecodecamp.org/learn/front-end-development-libraries/" color="teal.500" target="_blank">Front end dev libraries</Link></ListItem></Text>
                         </UnorderedList>
                     </TabPanel>
                     <TabPanel>
                         <Text textAlign="left">Click <Link href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
                             <ListItem>
-                                <Link href="https://www.udemy.com/course/javascript-code-exercise/" color="teal.500" target="_blank">Intro to JavaScript</Link>
+                                <Text textAlign="left"><Link href="https://www.udemy.com/course/javascript-code-exercise/" color="teal.500" target="_blank">Intro to JavaScript</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://www.udemy.com/course/react-node-js-web-development-projects/" color="teal.500" target="_blank">Intro to React</Link>
+                                <Text textAlign="left"><Link href="https://www.udemy.com/course/react-node-js-web-development-projects/" color="teal.500" target="_blank">Intro to React</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://www.udemy.com/course/8-beautiful-ruby-on-rails-apps-in-30-days/" color="teal.500" target="_blank">Intro to Ruby on Rails</Link>
+                                <Text textAlign="left"><Link href="https://www.udemy.com/course/8-beautiful-ruby-on-rails-apps-in-30-days/" color="teal.500" target="_blank">Intro to Ruby on Rails</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://www.udemy.com/course/csharp-tutorial-for-starter/" color="teal.500" target="_blank">Intro to C#.Net</Link>
+                                <Text textAlign="left"><Link href="https://www.udemy.com/course/csharp-tutorial-for-starter/" color="teal.500" target="_blank">Intro to C#.Net</Link></Text>
                             </ListItem>
                         </UnorderedList>
                     </TabPanel>
@@ -72,16 +72,16 @@ export const Resources=() => {
                         <Text textAlign="left">Click <Link href="https://github.com" color="teal.500" target="_blank">here</Link> to get started</Text>
                         <UnorderedList>
                             <ListItem>
-                                <Link href="https://pages.github.com/" color="teal.500" target="_blank">GitHub Pages</Link>
+                                <Text textAlign="left"><Link href="https://pages.github.com/" color="teal.500" target="_blank">GitHub Pages</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://learngitbranching.js.org/" color="teal.500" target="_blank">Learn Git Branching</Link>
+                                <Text textAlign="left"><Link href="https://learngitbranching.js.org/" color="teal.500" target="_blank">Learn Git Branching</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request" color="teal.500" target="_blank">Learn Pull Requests</Link>
+                                <Text textAlign="left"><Link href="https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request" color="teal.500" target="_blank">Learn Pull Requests</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://github.com/firstcontributions/first-contributions" color="teal.500" target="_blank">First Contributions</Link>
+                                <Text textAlign="left"><Link href="https://github.com/firstcontributions/first-contributions" color="teal.500" target="_blank">First Contributions</Link></Text>
                             </ListItem>
                         </UnorderedList>
                     </TabPanel>
@@ -89,13 +89,13 @@ export const Resources=() => {
                         <Text textAlign="left">Curated Collections</Text>
                         <UnorderedList>
                             <ListItem>
-                                <Link href="https://github.com/MunGell/awesome-for-beginners" color="teal.500" target="_blank">Awesome for beginners</Link>
+                                <Text textAlign="left"><Link href="https://github.com/MunGell/awesome-for-beginners" color="teal.500" target="_blank">Awesome for beginners</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://goodfirstissue.dev/" color="teal.500" target="_blank">First issues for beginners</Link>
+                                <Text textAlign="left"><Link href="https://goodfirstissue.dev/" color="teal.500" target="_blank">First issues for beginners</Link></Text>
                             </ListItem>
                             <ListItem>
-                                <Link href="https://up-for-grabs.net/" color="teal.500" target="_blank">Up for grabs issues</Link>
+                                <Text textAlign="left"><Link href="https://up-for-grabs.net/" color="teal.500" target="_blank">Up for grabs issues</Link></Text>
                             </ListItem>
                         </UnorderedList>
                     </TabPanel>
