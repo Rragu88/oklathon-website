313235549424cc54eef7e8d827b11e248cb92bf6
duonglamh1999 <duonglamh1999@gmail.com>
2024-10-19 14:50:37 -0500

    fix styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,24 +9,25 @@ export const AboutPage = () => {
         <>
             <Container
             maxW='1100px' 
-            height="100%" 
             display='flex'
             justifyContent='flex-start'
             flexDir='column'
+            width={{sm:"70%",md:"100%"}}
+            margin={{base:"0 0 0 0"}}
             >
-                <Text fontFamily="'Poppins', sans-serif" fontWeight="800" color='#b5e853' fontSize='9xl'  fontStyle='italic'>Oklathon
+                <Text fontFamily="'Poppins', sans-serif" fontWeight="800" color='#b5e853' fontSize={{sm:'6xl',md:'9xl'}}  fontStyle='italic'>Oklathon
                 </Text>
 
-                <Text fontSize='4xl' as='b' align='left' width='100%' >About Us
+                <Text fontSize={{sm:'2xl',md:'4xl'}} as='b' align='left' width='100%' >About Us
                 </Text>
 
-                <Text align='left' fontSize='lg' margin='5px 0px 20px  0px'>Oklathon is an open-source software hackathon organized by Techlahoma, a nonprofit dedicated to advancing the tech community in Oklahoma. Techlahoma creates opportunities for developers to collaborate, network, and contribute to open-source projects. Oklathon provides a platform for participants of all skill levels to work on innovative software solutions, learn new technologies, and gain hands-on experience with tools like GitHub. Through its events and initiatives, 
+                <Text align='left' fontSize='lg' margin='5px 0px 20px  0px' >Oklathon is an open-source software hackathon organized by Techlahoma, a nonprofit dedicated to advancing the tech community in Oklahoma. Techlahoma creates opportunities for developers to collaborate, network, and contribute to open-source projects. Oklathon provides a platform for participants of all skill levels to work on innovative software solutions, learn new technologies, and gain hands-on experience with tools like GitHub. Through its events and initiatives, 
                 <Link href='https://www.techlahoma.org/' isExternal>Techlahoma     
                     <ExternalLinkIcon mx='2px' />
                 </Link> 
                 fosters a thriving tech ecosystem in the region. </Text>
 
-                <Text fontSize='4xl' as='b' align='left' >Next Events</Text> 
+                <Text fontSize={{sm:'2xl',md:'4xl'}} as='b' align='left' >Next Events</Text> 
 
                 <Text align='left' marginBottom='20px'>
                 <b>Date:</b> October 19, 2024 <br></br>
