d7735d0d1bd7867f0b02d85b029621c4d0b56a3b
Brittany Pizarro <88292020+Pizaroll@users.noreply.github.com>
2024-10-19 13:21:42 -0500

    Provider and path added

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {  ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import PastProjects from './pages/PastProjects.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
   },
+  {
+    path: "/PastProjects",
+    element: <PastProjects/>,
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </StrictMode>,
 )
