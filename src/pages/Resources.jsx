801e83a690e02fe23f678f272ad27c368c99ed06
wellrestedmango <ericfassbender@proton.me>
2024-10-19 12:22:49 -0500

    Update Resources.jsx
    switching to <Link> from <a>

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -7,7 +7,7 @@ import {
     TabList,
     TabPanel,
     TabPanels,
-    textDecoration
+    Link
   } from '@chakra-ui/react'
 
 export const Resources=() => {
@@ -30,18 +30,35 @@ export const Resources=() => {
                 </TabList>
                 <TabPanels>
                     <TabPanel>
-                        <p>Click <a href="freecodecamp.org" style={{textDecoration:"underline blue"}} target="_blank">here</a> to get started</p>
+                        <p>Click <Link href="freecodecamp.org" color="teal.500" target="_blank">here</Link> to get started</p>
                         <ul>
-                            <li><a href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" style={{textDecoration:"underline blue"}} target="_blank">Javascript Algorithms</a></li>
-                            <li><a href="https://www.freecodecamp.org/learn/python-for-everybody/" style={{textDecoration:"underline blue"}} target="_blank">Python for everyone</a></li>
-                            <li><a href="https://www.freecodecamp.org/learn/front-end-development-libraries/" style={{textDecoration:"underline blue"}} target="_blank">Front end dev libraries</a></li>
+                            <li><Link href="https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/" color="teal.500" target="_blank">Javascript Algorithms</Link></li>
+                            <li><Link href="https://www.freecodecamp.org/learn/python-for-everybody/" color="teal.500" target="_blank">Python for everyone</Link></li>
+                            <li><Link href="https://www.freecodecamp.org/learn/front-end-development-libraries/" color="teal.500" target="_blank">Front end dev libraries</Link></li>
                         </ul>
                     </TabPanel>
                     <TabPanel>
-                        <p>Click <a href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" style={{textDecoration:"underline blue"}} target="_blank">here</a> to get started</p>
+                        <p>Click <Link href="https://www.udemy.com/?utm_source=adwords-brand&utm_medium=udemyads&utm_campaign=Brand-Udemy_la.EN_cc.US_dev&campaigntype=Search&portfolio=BrandDirect&language=EN&product=Course&test=&audience=Keyword&topic=&priority=NotSpecified&utm_content=deal4584&utm_term=_._ag_137319648178_._ad_634190764968_._kw_udemy_._de_c_._dm__._pl__._ti_kwd-296956216253_._li_9026222_._pd__._&matchtype=b&gad_source=1&gclid=EAIaIQobChMI3-v0-PGaiQMVzACtBh3QFimqEAAYASAAEgLRo_D_BwE" color="teal.500" target="_blank">here</Link> to get started</p>
                         <ul>
                             <li>
-
+                                <Link href="https://www.udemy.com/course/javascript-code-exercise/" color="teal.500" target="_blank">Intro to JavaScript</Link>
+                            </li>
+                            <li>
+                                <Link href="https://www.udemy.com/course/react-node-js-web-development-projects/" color="teal.500" target="_blank">Intro to React</Link>
+                            </li>
+                            <li>
+                                <Link href="https://www.udemy.com/course/8-beautiful-ruby-on-rails-apps-in-30-days/" color="teal.500" target="_blank">Intro to Ruby on Rails</Link>
+                            </li>
+                            <li>
+                                <Link href="https://www.udemy.com/course/csharp-tutorial-for-starter/" color="teal.500" target="_blank">Intro to C#.Net</Link>
+                            </li>
+                        </ul>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Click <Link href="https://github.com" color="teal.500" target="_blank">here</Link> to get started</p>
+                        <ul>
+                            <li>
+                                
                             </li>
                         </ul>
                     </TabPanel>
